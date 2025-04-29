5b1dbf7f1d4795704d03319125502ea6080d5cc5
Hanna Ponomarenko <pnmrnk.anna@gmail.com>
2025-04-29 20:00:19 +0200

    update

--- js/gallery.js
+++ js/gallery.js
@@ -71,7 +71,7 @@ const markup = images
     image => `
       <li class="gallery-item">
         <a class="gallery-link" href="${image.original}">
-          <img width="340"
+          <img 
             class="gallery-image"
             src="${image.preview}"
             data-source="${image.original}"
